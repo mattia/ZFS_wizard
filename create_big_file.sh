8abc5962ce69bd367b8f0933ae93a9f0fa0a0424
Mattia Valzelli <mattia.valzelli@gmail.com>
2013-07-01 15:28:51 +0000

    added script to create and modify file

---
+++ create_big_file.sh
@@ -0,0 +1,2 @@
+#! /usr/local/bin/zsh
+dd if=/dev/random of=/home/mattia/Desktop/test.txt bs=100M count=1
---
+++ modify_big_file.sh
@@ -0,0 +1,2 @@
+#! /usr/local/bin/zsh
+dd if=/dev/random bs=10M count=1 >> /home/mattia/Desktop/test.txt
